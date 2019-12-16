ca08ce4908cccb183214400a24d8158d6e25ed2b
rushabhdharia <rushabh.dharia@gmail.com>
2019-12-15 22:06:07 -0500

    Newest Version

---
+++ data_gen.py
@@ -0,0 +1,52 @@
+import os
+import cv2
+import pickle
+
+import numpy as np
+from tensorflow.keras.utils import Sequence
+from tensorflow.keras.preprocessing.sequence import TimeseriesGenerator
+
+
+class DataGenerator(Sequence):
+    
+    def __init__(self, x_path, y_path = None, to_fit = True,  seq_len = 30, batch_size = 4):
+        self.x_path = x_path        
+        self.batch_size = batch_size
+        self.to_fit = to_fit
+        self.list_X = os.listdir(self.x_path)
+        self.seq_len = seq_len
+        if to_fit:
+            self.y_path = y_path
+            self.dict_Y = self.get_y(y_path)
+    
+    
+    def __len__(self):
+        return len(self.list_X)
+    
+    
+    def __getitem__(self, index):
+        images_folder = self.list_X[index]
+        images_list = sorted(os.listdir(self.x_path + images_folder))
+        all_frames = []
+        for img in images_list:
+            all_frames.append(np.array(cv2.imread(x_train_path+images_folder+'/'+img)))
+        
+        all_frames = np.stack(all_frames)
+        
+        key = images_folder.split('_')[:2]
+        key = '_'.join(key)
+        Y = np.array(self.dict_Y[key])
+        all_frames, targets = self.check(all_frames, Y)
+        series_data = TimeseriesGenerator(all_frames, targets, length = self.seq_len, batch_size=batch_size)
+        
+        return series_data
+    
+    def get_y(self, path):
+        with open(path, 'rb') as pickle_file:
+            y_dict = pickle.load(pickle_file)
+        return y_dict 
+    
+    def check(self, all_frames, targets):
+        if all_frames.shape[0] < targets.shape[0]:
+            targets = targets[:-1]
+        return all_frames, targets
---
+++ model.py
@@ -0,0 +1,50 @@
+from tensorflow.keras import Model
+from tensorflow.keras.layers import ConvLSTM2D, Attention, Dense, Flatten, MaxPool3D, MaxPool2D, BatchNormalization, LayerNormalization
+
+
+class Encoder(Model):
+    def __init__(self):
+        super(Encoder, self).__init__()
+        self.conv3d_1 = Conv3D(2, kernel_size = (1,7,7), padding = 'same')
+        self.batchnorm_1 = BatchNormalization()
+        self.batchnorm_2 = BatchNormalization()
+        self.batchnorm_3 = BatchNormalization()
+        self.maxpool3d = MaxPool3D(pool_size=(1,2,2))
+        self.conv3d_2 = Conv3D(4, kernel_size = (1,5,5), padding = 'same')
+        self.conv3d_3 = Conv3D(8, kernel_size = (1,3,3), padding = 'same')
+
+
+    def call(self, inputs):
+        x = self.conv3d_1(inputs)
+        x = self.batchnorm_1(x)
+        x = self.maxpool3d(x)
+        x = self.conv3d_2(x)
+        x = self.batchnorm_2(x)
+        x = self.maxpool3d(x)
+        x = self.conv3d_3(x)
+        x = self.batchnorm_3(x)
+        x = self.maxpool3d(x)
+        return x
+
+class MyCL_Model(Model):
+    
+    def __init__(self):
+        super(MyCL_Model, self).__init__()
+        self.encoder = Encoder()
+        self.convlstm_1 = ConvLSTM2D(filters=16, kernel_size=(3, 3), strides = (2, 2) ,padding='valid', return_sequences=False)
+        self.layernorm = LayerNormalization()
+        self.maxpool2d = MaxPool2D()
+        self.flatten = Flatten()
+        self.dense_1 = Dense(100, activation = 'relu')
+        self.classifier = Dense(6, activation = 'softmax') 
+
+        
+    def call(self, inputs):
+        x = self.encoder(inputs)
+        x = self.convlstm_1(x)
+        x = self.layernorm(x)
+        x = self.maxpool2d(x)
+        x = self.flatten(x)
+        x = self.dense_1(x)
+
+        return self.classifier(x)
